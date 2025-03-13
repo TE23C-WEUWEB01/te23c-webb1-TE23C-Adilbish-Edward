1c47e2f012d625f41d1d9a1eca1cddb684d700a1
edward.adilbishvile <edward.adilbishvilen@elev.ga.ntig.se>
2025-03-13 14:26:36 +0100

    Tränat på javaskript, variabler, konkatenering

---
+++ kapitel-8/intro-js/script.js
@@ -0,0 +1,16 @@
+// var tal1 = 5;
+// var tal2 = 7;
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa)
+
+var förnamn = "Kamala";
+var efternamn = "Kaangasa";
+var ålder = 0.1;
+var namn = förnamn + " " + efternamn;
+// var meddelande = namn + " är " + ålder + " år gammal. ";
+// alert(meddelande);
+
+var hobby = "bönutrop";
+var kostnad = "0kr";
+var meddelande = namn + " gillar " + hobby + " och det kostar " + kostnad + " per år.";
+alert(meddelande);
